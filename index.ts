695d57cec723b95cca2d41b4497ec5c3f9c84e11
Ambreen Sheikh <ambreensheikh900@gmail.com>
2024-06-02 21:29:39 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+//ARRAY SYNTAX
+ let favFruits:string[] = ["mango" , "apple" , "pear" , "kiwi"]
+  console.log(favFruits);
+
+//  //ARRAY INDEX(0,1,2,....)
+  const studNames:string[]= ["Ali", "Umer","Aina","Dua"]
+  console.log(studNames[1]);
+
+ //ARRAY 2
+ let array2 : Array = [1, 2, 3]//  apply Array<t> to crrect syntax
+
+ //ARRAY 3
+ let array3 :number[] = []     //  correct syntax of empty array
+
+ //ARRAY 4
+ let array4 : number[] =new number[3]    //refers to a type not as a value
+
+//ARRAY 5
+let array5 :number[]= [];
+array5.push(1234);        //dynamically added
+console.log(array5);
+
+
